95b4ed1d93fc693f0c0d08a34eb07785fc3a60e0
Greg Santos <gregsantos@gmail.com>
2019-12-08 11:59:16 -0800

    Add constants, state vars, cached DOM elements and event listeners

--- js/main.js
+++ js/main.js
@@ -1,9 +1,31 @@
 /*----- constants -----*/
+const LOSE_WRONG_COUNT = 6
+const SPRITE_WIDTH = 75
+const WORDS = [
+  'DEVELOPER',
+  'HTML',
+  'JAVASCRIPT',
+  'PROGRAM',
+  'CODE',
+  'FUNCTION',
+  'RECURSION',
+  'OBJECT',
+  'ARRAY',
+]
 
 /*----- app's state (variables) -----*/
+let secretWord, guessWord, usedLetters, wrongLetters
 
-/*----- cached element references -----*/
+/*----- cached DOM element references -----*/
+const guessWordEl = document.getElementById('word')
+const stageEl = document.getElementById('stage')
+const messageEl = document.querySelector('h2')
+const letterBtns = document.querySelectorAll('#letters button')
+const replayBtn = document.getElementById('replay')
 
 /*----- event listeners -----*/
+document.getElementById('letters').addEventListener('click', handleLetterClick)
+
+replayBtn.addEventListener('click', init)
 
 /*----- functions -----*/
